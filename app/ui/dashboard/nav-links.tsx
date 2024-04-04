9c6c6785917b85ce634a9400a0eb61467d397bc9
potnar <potnar6@users.noreply.github.com>
2024-04-04 16:07:08 +0200

    init commit main

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -40,6 +40,7 @@ export default function NavLinks() {
             )}
           >
             <LinkIcon className="w-6" />
+            test
             <p className="hidden md:block">{link.name}</p>
           </Link>
         );
